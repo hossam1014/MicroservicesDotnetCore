f38a7b682dd9245fc0c832325d8772f13c27e54c
Hossam Mostafa <h.mustfa@yahoo.com>
2023-09-27 19:15:52 +0300

    26-9-2023

---
+++ src/BuildingBlocks/EventBus.Messages/Common/EventBusConstants.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Common
+{
+    public static class EventBusConstants
+    {
+        public const string BasketCheckoutQueue = "basketcheckout-queue";
+    }
+}
